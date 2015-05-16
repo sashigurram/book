8a07ac8d3dfcdffb7140313e7c0aa9e77743048c
andreas77 <andreashorni@hotmail.com>
2015-05-16 14:03:34 +0200

    bib per chapter

---
+++ latex2pdf - bib-per-chapter.bat
@@ -0,0 +1,16 @@
+xelatex main
+bibtex using/configuring -min-crossrefs=99
+bibtex using/introducing -min-crossrefs=99
+bibtex using/lgstarted -min-crossrefs=99
+bibtex using/scoring -min-crossrefs=99
+makeglossaries main
+xelatex main
+bibtex using/configuring -min-crossrefs=99
+bibtex using/introducing -min-crossrefs=99
+bibtex using/lgstarted -min-crossrefs=99
+bibtex using/scoring -min-crossrefs=99
+makeglossaries main
+makeindex main
+xelatex main
+xelatex main
+
