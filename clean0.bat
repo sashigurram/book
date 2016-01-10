d04eb1834e418b5218cbf4c5b9d5a85e71c4dd1f
andreas77 <andreashorni@hotmail.com>
2016-01-10 14:39:55 +0100

    and back for win10

---
+++ clean0.bat
@@ -0,0 +1,48 @@
+del *.aux
+del *.log
+del *.aux
+del *.bbl
+del *.blg
+del *.lof
+del *.lot
+del *.out
+del *.toc
+del *.aux
+del *.glg
+del *.glo
+del *.gls
+del *.idx
+del *.ilg
+del *.ind
+del *.ist
+del *.acn
+del *.acr
+del *.alg
+
+cd using
+del *.aux
+del *.bbl
+del *.blg
+
+cd ../extending
+del *.aux
+del *.bbl
+del *.blg
+
+cd modules
+del *.aux
+del *.bbl
+del *.blg
+
+cd ../../understanding
+del *.aux
+del *.bbl
+del *.blg
+
+cd ../scenarios
+del *.aux
+del *.bbl
+del *.blg
+
+
+
